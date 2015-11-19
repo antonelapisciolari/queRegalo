f4af0cf976b6a94f4d67cdc0c3e4ed3402330496
antopiscio <antonela.pisciolari@experian.com>
2015-11-19 14:29:43 -0300

    adding regalo

---
+++ js/actions.js
@@ -0,0 +1,26 @@
+function login ()
+{
+var valid = false
+var username = document.getElementById("user-email").value;
+var password = document.getElementById("user-pw").value;
+
+var unArray = ["fran@gmail.com", "agos@gmail.com", "anto", "admin"];  // as many as you like - no comma after final entry
+var pwArray = ["regalo", "regalo", "regalo", "admin"];  // the corresponding passwords;
+
+for (var i=0; i <unArray.length; i++) {
+if ((username == unArray[i]) && (password == pwArray[i])) {
+valid = true;
+}
+if (valid)
+{
+window.location = "main.html";
+}
+else
+{
+window.location = "index.html"; // Redirecting to other page.
+} 
+valid = false;
+}
+
+}	
+	
